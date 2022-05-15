794b99670c7733e0bb1c0283160c3c3bc5eb1689
Node Sans <node189@icloud.com>
2022-05-15 12:35:36 +0800

    算法修正完成

--- src/OS.cpp
+++ src/OS.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 namespace scheduler {
+using std::string;
 
 void OS::tick() { up_time++; }
 
@@ -27,9 +28,11 @@ void OS::startTicking() {
 }
 
 void OS::timeInterrupt() {
-  if (!current_process_pid.empty()) {
-    const auto &process =
-        processes.find(current_process_pid)->second; // 当前运行的进程
+  const auto process_it = processes.find(current_process_pid);
+  const auto has_running_process = process_it != processes.end();
+  const auto &process = process_it->second; // 当前运行的进程
+
+  if (has_running_process) {
     if (process->get_up_time() == process->get_execute_time()) {
       process->clear_up_time();
       process->inc_cycle_id();
@@ -38,7 +41,7 @@ void OS::timeInterrupt() {
 
   // 寻找当前松弛度最低的进程
   int min_laxity = minLaxityInit;
-  std::string urgent_process_pid;
+  string urgent_process_pid;
   for (auto &process_pair : processes) {
     const auto &process = process_pair.second;
     if (process->get_cycle_id() * process->get_cycle_time() > up_time) {
@@ -50,38 +53,47 @@ void OS::timeInterrupt() {
       urgent_process_pid = process->get_pid();
     }
   }
-  if (urgent_process_pid.empty()) {
+  if (urgent_process_pid.empty()) { // 所有进程都执行完了
+    current_process_pid = "";
     return;
   }
-  if (urgent_process_pid != current_process_pid) { // 进程抢占
+
+  auto can_preempt = false;
+  if (has_running_process) {
+    if (min_laxity == 0 || process->get_up_time() == 0) {
+      can_preempt = true;
+    }
+  } else {
+    can_preempt = true;
+  }
+  if (urgent_process_pid != current_process_pid && can_preempt) { // 进程抢占
     const auto &urgent_process = processes.find(urgent_process_pid)->second;
     std::cout << "任务名: " << urgent_process_pid << std::endl
               << "周期数: " << urgent_process->get_cycle_id() + 1 << std::endl
               << "调度时刻: " << up_time << std::endl
               << "运行持续时间: " << urgent_process->get_up_time() << std::endl
               << "====================" << std::endl;
+    current_process_pid = urgent_process_pid;
   }
-  current_process_pid = urgent_process_pid;
 
-  // 增加当前的进程的运行时间
-  const auto &process = processes.find(current_process_pid)->second;
-  process->inc_up_time();
+  // 增加经过竞争后的当前进程的运行时间
+  processes.find(current_process_pid)->second->inc_up_time();
 }
 
 void OS::start() {
   // 添加进程
-  const auto aCycleTime = 6;
-  const auto aExecuteTime = 2;
+  const auto aCycleTime = 20;
+  const auto aExecuteTime = 10;
 
-  const auto bCycleTime = 8;
-  const auto bExecuteTime = 2;
+  const auto bCycleTime = 50;
+  const auto bExecuteTime = 25;
 
-  const auto cCycleTime = 10;
-  const auto cExecuteTime = 3;
+  // const auto cCycleTime = 45;
+  // const auto cExecuteTime = 12;
 
   add_process(process_ptr(new Process("A", aCycleTime, aExecuteTime)));
   add_process(process_ptr(new Process("B", bCycleTime, bExecuteTime)));
-  add_process(process_ptr(new Process("C", cCycleTime, cExecuteTime)));
+  // add_process(process_ptr(new Process("C", cCycleTime, cExecuteTime)));
 
   // 开始记时
   startTicking();
